9340ddcde5bcbdbfb45a067d38cd2340b78ce24f
kyawthiha75 <48701264+kyawthiha75@users.noreply.github.com>
2019-03-18 21:05:55 -0700

    Update and rename FIFO.v to FIFO_wtodelay.v

--- FIFO_wtodelay.v
+++ FIFO_wtodelay.v
@@ -67,9 +67,7 @@ write:
 	 if( wt_cycle != rd_cycle)
 	 full = 1'b1;
 	 else
-	 ;
-	//@(posedge clk_in) temp1 = 1'b0;
-	//@(posedge clk_in) empty = temp1;	
+	 ;	
 	empty = 1'b0;
 	wt_next_state = wt_idle;
 	
@@ -139,9 +137,7 @@ read:
 	if( rd_pointer == wr_prointer)
 	 if( rd_cycle == wt_cycle)	 
 	 empty = 1'b1;	
-	 else ;
-	//@(posedge clk_in) temp2 = 1'b0;
-	//@(posedge clk_in) full = temp2;	
+	 else ;		
 	full = 1'b0;
 	rd_next_state = rd_idle;
 	
